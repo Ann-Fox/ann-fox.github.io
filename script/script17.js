6160d5f1f82ccb1f537f77de6dce64b471b9bf86
Anna Chernysheva <101977767+Ann-Fox@users.noreply.github.com>
2022-06-07 20:20:59 +0500

    domashka17 all end

--- script/script17.js
+++ script/script17.js
@@ -16,4 +16,54 @@ console.log(all_li);
 
 //   2.5. Получить родителя первого элемента списка (closest)
 const lili = document.getElementsByTagName('li');
-const parentLi = lili.closest('list');
+const parentLi = lili[0].closest('.list');
+const parentLi2 = lili[1].closest('.list__parents');
+
+console.log(parentLi);
+console.log(parentLi2);
+
+console.log(lili[0]);
+
+const parentFather = document.getElementById('father');
+const parentLi3 = parentFather.parentNode;
+console.log(parentLi3);
+
+
+//   3. Добавить во все элементы списка произвольный текст
+all_li.forEach(element => {
+  element.textContent += " [my appended text 😎]";
+});
+
+
+// 4.1. Создать пустой html-узел для абзаца и добавить в него любой текст.
+let parNew = document.createElement('p'); 
+console.log(parNew);
+parNew.textContent = 'Hello world!';
+
+//   4.2. Добавить созданный абзац последним элементом в body
+const body = document.querySelector('body');
+setTimeout(()=>body.append(parNew), 3000);
+
+// 5.1. Для созданного ранее абзаца, добавить два любых класса
+parNew.classList.add('new-class', 'new-class-bg');
+
+// 5.2. Удалить класс у созданного ранее списка элементов
+
+
+// 6. С помощью js изменить цвет текста для всех  элементов списка и цвет заливка для абзаца
+
+all_li.forEach(element => {
+  element.style.color = "red";
+})
+
+// setTimeout(() => all_ul.style.color = "green", 3000);
+
+setTimeout(() =>
+ all_ul.forEach(  element => {
+  element.style.color = "green";
+}), 3000);
+
+setTimeout(() => parNew.style.backgroundColor = 'blue', 4000);
+
+
+
